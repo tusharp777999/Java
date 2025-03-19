e9ac892e4e09f385eaa07eacb60f68aa69d2396e
tushar-patil <tushar@veracity-india.com>
2025-03-19 14:26:57 +0530

    Sevlet Programs

---
+++ 1.FirstServlet/src/main/java/com/tushar/FirstServlet.java
@@ -0,0 +1,44 @@
+package com.tushar;
+
+import java.io.IOException;
+
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+public class FirstServlet implements Servlet{
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("I am in service");
+		
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ 2.SecondServlet/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<a href="secondServlet">Click Me</a>
+</body>
+</html>
