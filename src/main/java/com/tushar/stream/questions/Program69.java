1ca552bdc547f51c8919850c2cc95ad3b784df17
tushar <tusharp777999@gmail.com>
2026-01-28 00:45:54 +0530

    Added some new programs with question only solution is not added yet.

---
+++ src/main/java/com/tushar/stream/questions/Program69.java
@@ -0,0 +1,12 @@
+package com.tushar.stream.questions;
+
+/*
+* Given a sentence, need to find a second highest occurence of character. But if there is situation where multiple second-highest characters count then consider
+* I/P: "aaabbc"
+* O/P: b=2 or b
+*/
+public class Program69 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/tushar/stream/questions/Program70.java
@@ -0,0 +1,12 @@
+package com.tushar.stream.questions;
+
+/*
+* Given a string find out the vowels in it with its index.
+* I/P: "Tushar Patil"
+* O/P: u:1, a:4, a:8, i:10
+*/
+public class Program70 {
+    public static void main(String[] args) {
+
+    }
+}
