048140c88a62303f7fd0f70a172fedfc4ce435ec
tusharp777999 <56042362+tusharp777999@users.noreply.github.com>
2026-09-20 04:39:44 +0530

    Add examples of division by zero in Question.java

---
+++ Question.java
@@ -0,0 +1,4 @@
+System.out.println(10 / 0);     // ArithmeticException
+System.out.println(10.0 / 0);   // Infinity
+System.out.println(10.0 / 0.0); // Infinity
+System.out.println(0.0 / 0.0); // NaN
